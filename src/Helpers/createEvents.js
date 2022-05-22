05fb63ddfc58a7481c3fd09311bf06fcb48eec12
André Luna <drehluna@MacBook-Air-de-Andre.local>
2022-05-22 16:38:50 -0300

    Change logic in createEvents

--- src/Helpers/createEvents.js
+++ src/Helpers/createEvents.js
@@ -3,12 +3,9 @@ import { GenerateRandomDate} from "./generateRandomDate";
 
 export const pupoluteCalendar = async () => {
     const result = await getCharacters()
-
-    const EventsArray = []
-
     const { results } = result.data
 
-    results.forEach((item) => EventsArray.push({id: item.id, title: item.name, start: GenerateRandomDate(), end: GenerateRandomDate()}))
-  
-    return EventsArray
+    const events = results.map((item) => ({id: item.id, title: item.name, start: GenerateRandomDate(), end: GenerateRandomDate()}))
+    
+    return events
 }
