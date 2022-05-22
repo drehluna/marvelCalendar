6746ee53aaea0b9e8e67dce5b3a3ce802bc0648f
André Luna <drehluna@MacBook-Air-de-Andre.local>
2022-05-22 16:34:44 -0300

    Change services

--- src/services/marvelCharacters.js
+++ src/services/marvelCharacters.js
@@ -8,7 +8,7 @@ export const getCharacters = async () => {
     const publicKey = '1f486feacd3a6974101c6cc0724330cc'
     const privateKey = '883b9bd07d139adc02534e83416b0d535b62630d'
     const hash = CryptoJS.MD5(timesTemp + privateKey + publicKey).toString()
-    const url = `http://gateway.marvel.com/v1/public/characters?limit=20&ts=${timesTemp}&apikey=${publicKey}&hash=${hash}`
+    const url = `https://gateway.marvel.com/v1/public/characters?limit=20&ts=${timesTemp}&apikey=${publicKey}&hash=${hash}`
     
     const response = axios.get(url)
     .then((res) => res.data)
